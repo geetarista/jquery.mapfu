2bb2e662f33626dfcb9e1a20d2b3ccb3cd47f970
Robby Colvin <geetarista@gmail.com>
2010-02-07 08:21:13 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,78 @@
+# jQUery mapit plugin
+
+## Description
+
+jQuery mapit is a simple plugin to allow easy Google Map creation using version 3 of the JavaScript API. This means that an API key is not needed, but it also means that only features supported in V3 of the API are available to the plugin. Google is continually updating and adding new features to the API. I will continue to add more until the plugin supports all features in the V3 API and hopefully keep up with Google as they make changes.
+
+## Usage
+
+First, include jQuery, the Google Map JavaScript API, and this plugin:
+
+  <script type='text/javascript' src='http://maps.google.com/maps/api/js?sensor=false'></script>
+  <script type='text/javascript' src='jquery.js'></script>
+  <script type='text/javascript' src='jquery.mapit.js'></script>
+
+Next, include a div that will hold the map:
+
+  <div id='map'></div>
+
+Now just call the mapit function on a DOM element. It is usually best to do this once the document is ready.
+
+  $("#map").mapit();
+
+Since this just creates an empty map of the US, options must be passed to add desired functionality.
+
+## Examples
+
+[http://github.com/geetarista/jquery.mapit/tarball/master](Download the project), then open examples.html in your browser to see a few examples.
+
+## Options
+
+The following options are available when calling the plugin. Defaults are shown in parentheses.
+
+    data ([])
+The objects that will be passed to create the markers and infowindows. As long as valid JSON is passed to this option, it doesn't care how it comes in. It could be manually written out, pulled from a back-end, or pulled in via getJSON and then passing it to the plugin. The only requirements are (1) include a root element that bundles all of the sub-elements in and (2) objects respond to "lat" and "lng" for the latitude and longitude values, respectively. I'm hoping to add a feature so this isn't a requirement and there are options to set these.
+
+    latitude (null)
+The central latitude point for the map.
+
+    longitude (null)
+The central longitude point for the map.
+
+    title (function() {})
+Function used to pass a string to the marker that is displayed on hover.
+
+    html (function() {})
+Function used to pass a string of HTML to an infowindow that is opened when the marker is clicked.
+
+    zoom (8)
+Set the zoom level of the map using an integer.
+
+    mapType ("roadmap")
+Sets the type of map to display. Available options are "roadmap", "satellite", "hybrid", "terrain".
+
+    scrollwheel (true)
+If false, disables scrollwheel zooming on the map.
+
+    navigationControl (true)
+If false, the pan/zoom control is hidden.
+
+    mapTypeControl (true)
+If false, the control to change map types is hidden.
+
+    scaleControl (false)
+If true, shows the map scale control.
+
+    icon
+A hash containing options for marker icons. See below for actual options.
+
+    icon.image ('http://www.google.com/mapfiles/marker.png')
+Reference to an image that is used for your markers.
+
+## Examples
+
+Download examples.html and view it in your browser.
+
+## License
+
+jQuery mapit is under by the MIT License. See LICENSE for more information.
---
+++ jquery.mapit.js
@@ -0,0 +1,132 @@
+/**
+ * jQuery mapit Plugin
+ * Version: 1.0.0
+ * URL: http://github.com/geetarista/jquery.mapit
+ * Description: Easy integration with Google Maps (JS API V3).
+ * Usage: See README for more information.
+ * Requires: jQuery 1.3 or higher (may work below 1.3; just not tested)
+ * Author: Robby Colvin
+ * http://robbycolvin.com
+ * Copyright: Copyright 2009 Robby Colvin
+ * Licensed under MIT (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+(function($) {
+
+  $.fn.mapit = function(options) {
+
+    // Set up opts hash
+    var opts = $.extend({}, $.fn.mapit.defaults, options);
+
+    // Cycle through each DOM element invoked with plugin
+    return this.each(function() {
+
+      // Set the root key for the json object since it is unkown
+      // TODO: See if there is another way to accomplish this
+      var keys = [];
+      for (key in opts.data[0]) {
+        keys.push(key);
+      }
+      var rootKey = keys[0];
+
+      // Initiate array to hold marker html contents
+      var htmlArray = [];
+
+      // Set latitude and longitude if no option is given
+      if (opts.latitude == null && opts.longitude == null) {
+        // Set lat/lng for first object if exists
+        if (opts.data.length > 0) {
+          opts.latitude = opts.data[0][rootKey].lat;
+          opts.longitude = opts.data[0][rootKey].lng;
+        } else {
+          // Set default lat/lng to US center
+          opts.latitude = 37.0625;
+          opts.longitude = -95.677068;
+        }
+      }
+
+      // Map options when initializing map object
+      // API requires this and cannot be done dynamically
+      var mapOptions = {
+        scrollwheel: opts.scrollwheel,
+        navigationControl: opts.navigationControl,
+        mapTypeControl: opts.mapTypeControl,
+        scaleControl: opts.scaleControl
+      }
+
+      // Initialize Google Map object
+      var map = new google.maps.Map(this, mapOptions);
+
+      // Set center of map
+      map.setCenter(new google.maps.LatLng(opts.latitude, opts.longitude));
+
+      // Set zoom level for map
+      map.setZoom(opts.zoom);
+
+      // Set type of map
+      switch(opts.mapType) {
+        case "satellite":
+          map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
+          break;
+        case "hybrid":
+          map.setMapTypeId(google.maps.MapTypeId.HYBRID);
+          break;
+        case "terrain":
+          map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
+          break;
+        default:
+          map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+      }
+
+      // Set up infowindow object
+      var infowindow = new google.maps.InfoWindow();
+
+      // Iterate through data and create a marker for each item
+      // TODO: Add further support for icon options
+
+      // If opts.data contains elements
+      if (opts.data.length > 0) {
+        // Iterate through each object found in JSON
+        $.each(opts.data, function(i, item) {
+          var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(item[rootKey].lat, item[rootKey].lng),
+            map: map,
+            icon: opts.icon.image,
+            title: opts.title(item[rootKey]),
+            zIndex: i
+          });
+          
+          if (opts.html) {
+            // Add html for this marker to array
+            htmlArray[i] = opts.html(item[rootKey]);
+
+            // Create a click event listener for this marker and open infowindow
+            google.maps.event.addListener(marker, 'click', function() {
+              infowindow.setContent(htmlArray[i]);
+              infowindow.open(map, marker);
+            });
+          }
+        }); // end $.each function
+      }
+
+    }); // end return this.each function
+  }; // end $.fn.mapit
+
+  // Set plugin defaults
+  $.fn.mapit.defaults = {
+    latitude: null,
+    longitude: null,
+    navigationControl: true,
+    mapTypeControl: true,
+    scaleControl: false,
+		zoom: 8,
+    data: [],
+		scrollwheel: true,
+    icon: {
+      image: 'http://www.google.com/mapfiles/marker.png'
+		},
+    html: function() {},
+    title: function() {}
+  }; // end $.fn.mapit.defaults
+
+})(jQuery); // end closure wrapper
